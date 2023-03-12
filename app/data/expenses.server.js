0ee5c9b6758ef75e06742f525fedf88582d08241
Charles Raviaud <charles.raviaud@protonmail.com>
2023-03-12 15:58:33 +0100

    feat: add default error handling logic

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -10,8 +10,7 @@ export async function addExpense(expenseData) {
       }
     });
   } catch (error) {
-    console.log(error);
-    throw error;
+    throw new Error('Failed to add expense.');
   }
 }
 
@@ -23,8 +22,7 @@ export async function getExpenses() {
       },
     });
   } catch (error) {
-    console.log(error);
-    throw error;
+    throw new Error('Failed to get expenses.');
   }
 }
 
@@ -33,8 +31,7 @@ export async function getExpense(id) {
     const expense = await prisma.expense.findFirst({ where: { id } });
     return expense;
   } catch (error) {
-    console.log(error);
-    throw error;
+    throw new Error('Failed to get expense.');
   }
 }
 
@@ -49,8 +46,7 @@ export async function updateExpense(id, expenseData) {
       },
     });
   } catch (error) {
-    console.log(error);
-    throw error;
+    throw new Error('Failed to update expense.');
   }
 }
 
@@ -60,7 +56,6 @@ export async function deleteExpense(id) {
       where: { id },
     });
   } catch (error) {
-    console.log(error);
-    throw error;
+    throw new Error('Failed to delete expense.');
   }
 }
--- app/root.jsx
+++ app/root.jsx
@@ -72,7 +72,20 @@ export function CatchBoundary() {
   )
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }) {
+  return (
+    <Document title='An error occured'>
+      <main>
+        <Error title='An error occured'>
+          <p>
+            {error.message ||
+              'Something went wrong. Please try again later.'}
+          </p>
+          <p>Back to <Link to='/'>safety</Link></p>
+        </Error>
+      </main>
+    </Document>
+  )
 }
 
 export function links() {
