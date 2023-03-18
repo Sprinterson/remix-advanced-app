bbee573fc3d25f88309790bd1feea838e2af0afb
Charles Raviaud <charles.raviaud@protonmail.com>
2023-03-18 23:52:58 +0100

    feat(auth.server): create user login

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -1,4 +1,4 @@
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 
 import { prisma } from './database.server';
 
@@ -17,4 +17,26 @@ export async function signup({ email, password }) {
   const passwordHash = await hash(password, 12);
 
   await prisma.user.create({ data: { email: email, password: passwordHash } })
+}
+
+export async function login({ email, password }) {
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+
+  if (!existingUser) {
+    const error = new Error(
+      'Could not log you in, please check the provided credentials'
+    );
+    error.status = 401;
+    throw error;
+  }
+
+  const passwordCorrect = await compare(password, existingUser.password);
+
+  if (!passwordCorrect) {
+    const error = new Error(
+      'Could not log you in, please check the provided credentials'
+    );
+    error.status = 401;
+    throw error;
+  }
 }
