bed42ff7163b8d3d5232007ac220a0a0b8e2c16a
Charles Raviaud <charles.raviaud@protonmail.com>
2023-03-18 23:07:14 +0100

    feat(auth.server): check for existing users & emails

---
+++ app/data/auth.server.js
@@ -0,0 +1,11 @@
+import { prisma } from './database.server';
+
+export async function signup({ email, password }) {
+  const existingUser = prisma.user.findFirst({ where: { email } });
+
+  if (existingUser) {
+    const error = new Error('A user with the provided email addresss exists already');
+    error.status = 422;
+    throw error;
+  }
+}
